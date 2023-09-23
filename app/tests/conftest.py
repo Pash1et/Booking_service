a4c225742eb0301cbbbd547205a3793c5b9f063b
Pash1et_ <d.gorelov22@gmail.com>
2023-09-23 21:10:19 +0300

    Начал писать интеграционные и юнит тесты

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,8 +1,10 @@
 import asyncio
-from datetime import datetime
 import json
+from datetime import datetime
 
 import pytest
+from fastapi.testclient import TestClient
+from httpx import AsyncClient
 from sqlalchemy import insert
 
 from app.bookings.models import Bookings
@@ -10,6 +12,7 @@ from app.config import settings
 from app.database import Base, async_session_maker, engine
 from app.hotels.models import Hotels
 from app.hotels.rooms.models import Rooms
+from app.main import app as fastapi_app
 from app.users.models import Users
 
 
@@ -31,14 +34,8 @@ async def prepare_database():
     bookings = open_mock_json("bookings")
 
     for booking in bookings:
-        booking["date_from"] = datetime.strptime(
-            booking["date_from"],
-            "%Y-%m-%d"
-        )
-        booking["date_to"] = datetime.strptime(
-            booking["date_to"],
-            "%Y-%m-%d"
-        )
+        booking["date_from"] = datetime.strptime(booking["date_from"], "%Y-%m-%d")
+        booking["date_to"] = datetime.strptime(booking["date_to"], "%Y-%m-%d")
 
     async with async_session_maker() as session:
         add_hotels = insert(Hotels).values(hotels)
@@ -59,3 +56,15 @@ def event_loop(request):
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
+
+
+@pytest.fixture(scope="session")
+async def async_client():
+    async with AsyncClient(app=fastapi_app, base_url="http://test") as client:
+        yield client
+
+
+@pytest.fixture(scope="session")
+async def session():
+    async with async_session_maker() as session:
+        yield session

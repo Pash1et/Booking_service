909b841f8df443ab396da5821e1a7f84e4ccb74a
Дмитрий Горелов <Pash1et_>
2023-10-26 21:31:21 +0300

    .

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,21 +1,51 @@
+import { useState } from "react";
+import axios from 'axios';
 import s from "./Login.module.css";
 
 export default function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        axios.post('http://api.solved-task.ru/auth/login', { email, password })
+            .then((response) => {
+                console.log("SUCCESS LOGIN")
+            })
+            .catch((err) => {
+                console.error("Error: ", err)
+            })
+    }
+
     return (
         <div className={s.login_page}>
             <div className={s.form_login_wrapper}>
                 <div className={s.title}>
                     Сервис бронирований
                 </div>
-                <div className={s.login_form}>
-                    <label for="email">Email</label>
-                    <input type="email" />
-                    <label for="password">Password</label>
-                    <input type="password" />
-                    <button>
+                <form className={s.login_form} onSubmit={handleSubmit}>
+                    <label for="email">
+                        Email:
+                        <input type="email" value={email} onChange={handleEmailChange} />
+                    </label>
+                    <label for="password">
+                        Password:
+                        <input type="password" value={password} onChange={handlePasswordChange} />
+                    </label>
+                    <button type="submit">
                         Войти
                     </button>
-                </div>
+                </form>
             </div>
         </div>
     );
